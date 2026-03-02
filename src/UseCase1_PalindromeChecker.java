99ce7eee808e56ccd58564e42233cedcbc8f821d
DuaaMohdAli <duamohdali@gmail.com>
2026-03-02 09:04:19 +0530

    Base application setup

---
+++ src/UseCase1_PalindromeChecker.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class UseCase1_PalindromeChecker {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management Systems ");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
