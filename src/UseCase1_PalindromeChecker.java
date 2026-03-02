4859b7bba96f052bf9717619489d1ac62000e48f
DuaaMohdAli <duamohdali@gmail.com>
2026-03-02 10:19:33 +0530

    Character Array Based Palindrome Check

--- src/UseCase1_PalindromeChecker.java
+++ src/UseCase1_PalindromeChecker.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 import java.util.Stack;
 public class UseCase1_PalindromeChecker {
     public static void main(String[] args){
-
-        String word = "madam";
-        String reversed="";
-
-        for(int i=word.length()-1;i>=0;i--){
-            reversed = reversed + word.charAt(i);
-        }
-
-        if(word.equals(reversed)){
-            System.out.println("It is a Palindrome ? : True");
-        }else{
-            System.out.println("It is a Palindrome ? : False");
-        }
+       String word = "radar";
+       char[] characters = word.toCharArray();
+       int left = 0;
+       int right = characters.length-1;
+       boolean isPalindrome = true ;
+       while(left<right){
+           if(characters[left]!=characters[right]){
+               isPalindrome = false ;
+               break;
+           }
+           left++;
+           right--;
+       }
+       System.out.println("Word: " + word);
+       System.out.println("Is it a Palindrome? " + isPalindrome);
     }
 }
